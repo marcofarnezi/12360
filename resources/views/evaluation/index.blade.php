aa4c42b4e73c14425a857a7b860f3375dd4ee3e7
Marco Farnezi <marcofarnezi@gmail.com>
2019-05-17 10:24:00 -0300

    Tela de respostas

--- resources/views/evaluation/index.blade.php
+++ resources/views/evaluation/index.blade.php
@@ -7,18 +7,18 @@
 
             <ul class="nav nav-tabs">
                 @foreach ($users as $user)
-                    <li><a href="#tab{{$user->id}}" data-toggle="tab">{{$user->name}}</a></li>
+                    <li><a href="#" data-id="tab{{$user->id}}" class="tab_user">{{$user->name}}</a></li>
                 @endforeach
 
             </ul>
             <div class="tab-content">
                 @foreach ($users as $user)
-                    <div class="tab-pane" id="tab{{$user->id}}">
+                    <div class="tab-pane tab_pane_user" id="tab{{$user->id}}">
                         <div class="tab-content">
                             <div class="tab-pane fade in show active">
                                 <div class="row">
                                     <div class="col-lg-10 col-md-12">
-                                        <div class="section-heading mb-4">{{ $form->title }}</div>
+                                        <div class="section-heading mb-4">{{ $form->title }} - {{$user->name}}</div>
                                         <table class="table table-striped table-responsive-md btn-table">
                                             <tr>
                                                 <th>
@@ -40,6 +40,19 @@
                                                             3<input type="radio" name="nota[{{ $question->id }}]" value="3">
                                                             4<input type="radio" name="nota[{{ $question->id }}]" value="4">
                                                             5<input type="radio" name="nota[{{ $question->id }}]" value="5">
+                                                        </td>
+                                                    </tr>
+                                                @endif
+                                            @endforeach
+                                            @foreach ($questions as $question)
+                                                @if ($question->type != 0)
+                                                    <tr colspan="2">
+                                                        <td>
+                                                            {{ $question->question }}
+                                                            <br>
+                                                            <textarea wight="100%">
+
+                                                            </textarea>
                                                         </td>
                                                     </tr>
                                                 @endif
@@ -69,4 +82,12 @@
 
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script>
+        $( document ).ready(function() {
+            $('.tab_user').click(function () {
+                $('.tab_pane_user').hide()
+                $('#' + $(this).data('id')).show()
+            })
+        });
+    </script>
 @endsection
